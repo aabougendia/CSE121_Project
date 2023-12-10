4658314fa0e18a050ee5a781975877d35880c4fd
aabougendia <22-101194@students.eui.edu.eg>
2023-12-10 23:28:02 +0200

    Adding Project files

---
+++ binary_tree.h
@@ -0,0 +1,9 @@
+typedef struct Node{
+    char* Q;
+    struct Node* y;
+    struct Node* n;
+} Node;
+
+typedef struct Binary_tree{
+    Node* root;
+} Binary_tree ;
---
+++ game.c
@@ -0,0 +1,11 @@
+///////////////////////// std header files
+#include <stdio.h>
+#include <stdlib.h>
+/////////////////////////  our header files
+#include "binary_tree.h"
+#include "play.h"
+#include "learn.h"
+////////////////////////  main()
+int main() {
+    return 0;
+}
