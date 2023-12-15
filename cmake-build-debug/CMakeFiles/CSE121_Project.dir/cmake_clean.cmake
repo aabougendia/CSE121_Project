cfb36fba990a9c5277f683d0d3266a2e05d9d08c
Salah-elsayed-2005 <118000804+Salah-elsayed-2005@users.noreply.github.com>
2023-12-15 17:53:03 +0200

    ز

---
+++ cmake-build-debug/CMakeFiles/CSE121_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSE121_Project.dir/game.c.obj"
+  "CMakeFiles/CSE121_Project.dir/game.c.obj.d"
+  "CSE121_Project.exe"
+  "CSE121_Project.exe.manifest"
+  "CSE121_Project.pdb"
+  "libCSE121_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CSE121_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
