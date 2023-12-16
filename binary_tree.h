58b8968f9c51382112c88843105dcca6aa30fceb
aabougendia <22-101194@students.eui.edu.eg>
2023-12-16 08:35:51 +0200

    final touches on binary_tree.h

--- binary_tree.h
+++ binary_tree.h
@@ -1,48 +1,52 @@
 #define MAX_LINE_SIZE 999
-typedef struct Node{
+
+typedef struct Node{    // Node structure definition
     char* data;
-    struct Node* y;
-    struct Node* n;
+    struct Node* y;     // yes link
+    struct Node* n;     // no link
 } Node;
 
-typedef struct Binary_tree{
-    Node* root;
+typedef struct Binary_tree{     // Binary tree structure definition
+    Node* root;     // root node
 } Binary_tree ;
 
-FILE* open_file(){
+FILE* open_file(){      // function to open the text file
     char* file_name = "countries.txt";
     FILE* filePtr = fopen(file_name, "r");
-    if(!filePtr) {
+    if(filePtr == NULL) {       // checking if the file opens successfully
         perror("Error encountered while opening file!\n");
         exit(-1);
     }
-    return filePtr;
+    return filePtr; // returning text file pointer
 }
 
-char* read_line(FILE* filePtr){
+char* read_line(FILE* filePtr){     // function to read the text file line by line
     char* line = (char*)malloc(MAX_LINE_SIZE * sizeof (char));
-    if(fgets(line, MAX_LINE_SIZE, filePtr) == NULL){
-        free(line);
-        return NULL;
+    if(fgets(line, MAX_LINE_SIZE, filePtr) == NULL){    //  checking if errors occur while reading text lines
+        free(line);     // deallocating line pointer if errors occur
+        return NULL;    // returning NULL pointer to indicate that error occured
     }
-    if(!strcmp(line, "-1\n"))
+    if(!strcmp(line, "-1\n"))   // checking if the line contains -1 which indicates reaching a leaf node
         return NULL;
-    return line;
+    return line
 }
 
-void construct_binary_tree(FILE* filePtr, Node** currentPtr){
-    char* line = read_line(filePtr);
-    if(line == NULL)
+void construct_binary_tree(FILE* filePtr, Node** iteratorPtr){  // a recursive function to construct the binary tree structure
+    char* line = read_line(filePtr);    // reading countries text file line by line
+    if(line == NULL)    // base case when we reach a leaf node
         return;
 
-    *currentPtr = (Node*) malloc(sizeof(Node));
+    *iteratorPtr = (Node*) malloc(sizeof(Node));    // allocating memory blocks for binary tree nodes
 
-    (*currentPtr)->data = line;
-    (*currentPtr)->y = NULL;
-    (*currentPtr)->n = NULL;
+    (*iteratorPtr)->data = line;    // storing text data
+    // initializing y and n linking pointers to NULL
+    (*iteratorPtr)->y = NULL;
+    (*iteratorPtr)->n = NULL;
 
-    construct_binary_tree(filePtr, &((*currentPtr)->y));
-    construct_binary_tree(filePtr, &((*currentPtr)->n));
+    // performing pre-order traversal over the binary tree using recursion function calling
+    construct_binary_tree(filePtr, &((*iteratorPtr)->y));
+    construct_binary_tree(filePtr, &((*iteratorPtr)->n));
 }
+
 
  
