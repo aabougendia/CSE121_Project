82d8c6dfb6faca425e80df84aab3e82afe0a0ccf
KareemYasser101 <kareemyassermoh@gmail.com>
2023-12-19 11:00:32 +0200

    Test it

--- learn.h
+++ learn.h
@@ -1,30 +1,30 @@
 
-void insertnode(char* oldcountry,char * newcountry,char* newquestion,Node* iteratorPtr){
+void insertnode(char* oldcountry,char * newcountry,char* newquestion,Node** iteratorPtr){
+    printf("infinite recursion\n");
+   if((*iteratorPtr)!=NULL) {
 
-   if(iteratorPtr!=NULL) {
-
-       if (iteratorPtr->y->data == oldcountry || iteratorPtr->n->data == oldcountry) {
-           if (iteratorPtr->y->data == oldcountry)
-               iteratorPtr->y->data = newquestion;
-           else if (iteratorPtr->n->data == oldcountry)
-               iteratorPtr->n->data = newquestion;
+       if ((*iteratorPtr)->y->data == oldcountry || (*iteratorPtr)->n->data == oldcountry) {
+           if ((*iteratorPtr)->y->data == oldcountry)
+               (*iteratorPtr)->y->data = newquestion;
+           else if ((*iteratorPtr)->n->data == oldcountry)
+               (*iteratorPtr)->n->data = newquestion;
 
            Node *Ynode = (Node *) malloc(sizeof(Node)); // creating new node for the Yes link ( New country )
-           iteratorPtr->y = Ynode;
+           (*iteratorPtr)->y = Ynode;
            Ynode->data = newcountry;
            Ynode->y = NULL;
            Ynode->n = NULL;
            Node *Nnode = (Node *) malloc(sizeof(Node));//creating new node for the No link ( old country )
-           iteratorPtr->n = Nnode;
+           (*iteratorPtr)->n = Nnode;
            Nnode->data = oldcountry;
            Nnode->y = NULL;
            Nnode->n = NULL;
-
+           return;
        }
        //pre-order traversal left
-       insertnode(oldcountry,newcountry,newquestion,(iteratorPtr)->y);
+       insertnode(oldcountry,newcountry,newquestion,&((*iteratorPtr)->y));
        //pre-order traversal right
-       insertnode(oldcountry,newcountry,newquestion,(iteratorPtr)->n);
+       insertnode(oldcountry,newcountry,newquestion,&((*iteratorPtr)->n));
 
    }
 
@@ -33,13 +33,13 @@ void insertnode(char* oldcountry,char * newcountry,char* newquestion,Node* itera
 }
 
 void Write_BinTree(FILE *fptr,Node * iterator){
-
+    printf("Entered write function");
     if(iterator!=NULL){
         fprintf(fptr,"%s\n",iterator->data);
         Write_BinTree(fptr,iterator->y);
         Write_BinTree(fptr,iterator->n);
     }
-    if(iterator == NULL)
+    else if(iterator == NULL)
         fprintf(fptr,"-1\n");
 }
 
@@ -47,9 +47,13 @@ void Write_BinTree(FILE *fptr,Node * iterator){
 
 void learn(Binary_tree countries,char* oldcountry,char * newcountry,char* newquestion){
 
-insertnode(oldcountry,newcountry,newquestion,countries.root);
-FILE * fptr = fopen("C:\\Users\\Kareem\\Desktop\\Kareem\\University\\Engineering - Year 2\\2- Advanced Programming\\Project\\Country-guessing-game repo\\CSE121_Project\\Write_NewBinTree.txt","r+");
-Write_BinTree(fptr,countries.root);
+insertnode(oldcountry,newcountry,newquestion,&countries.root);
+printf("\n***********************Exited insertion function");
+FILE * fptr = fopen("C:\\Users\\Kareem\\Desktop\\Kareem\\University\\Engineering - Year 2\\2- Advanced Programming\\Project\\Country-guessing-game repo\\CSE121_Project\\Write_NewBinTree.txt","w");
+if(fptr==NULL)
+    perror("Error encountered while opening file");
+else
+    Write_BinTree(fptr,countries.root);
 
 
 }
