2bd61e4ae6c23f7bd8eeef5400fab3982dac9982
Salah-elsayed-2005 <118000804+Salah-elsayed-2005@users.noreply.github.com>
2023-12-28 15:08:54 +0200

    Update play.h

--- play.h
+++ play.h
@@ -1,3 +1,4 @@
+// used extern to ger the 2 functions from learn.h
 extern void Write_BinTree(FILE *fPtr, Node * iterator);
 extern void learn(Node** root, char* new_country, char* new_question);
 void intro(){ // welcome message that only initializes at first round
@@ -7,7 +8,7 @@ void intro(){ // welcome message that only initializes at first round
 }
 // Function to read a single character input and consume the newline
 char getChoice() {
-    char choice = getchar();
+    char choice = getchar(); // get user answer
     choice = tolower(choice); // Convert to lowercase
     while (getchar() != '\n'); // Clear the input buffer
     return choice;
@@ -26,9 +27,15 @@ void credits(){
 }
 void endGame() {    // Function to handle the end of the game
     printf("\nThanks for Playing <3\n");
-    credits();
-    // exit(0);
-}
+    printf("---------------------------\n");
+    printf("This game was developed by:\n\n");
+    printf("Abdulrahman Abougendia  22-101194\n");
+    printf("SalahEldin El-Sayed     22-101188\n");
+    printf("Kareem Yasser           22-101124\n");
+    printf("Fouad Hashesh           22-101062\n");
+    printf("Ibrahim Ehab            22-101281\n");
+    printf("Mohamed Farouk          22-101284\n");
+ }
 
 // Play function - recursive approach to traverse the tree based on user input
 void play(Node* iterator, Node* original, Binary_tree countries) {
@@ -40,17 +47,17 @@ void play(Node* iterator, Node* original, Binary_tree countries) {
     if(iterator == original){ // welcome function
         intro();
     }
-
-    char ans;
-
+     char ans; // A variable to get any user input
+    label:
     // Leaf node - guess the country
     if (iterator->y == NULL && iterator->n == NULL) {
         printf("Is your country %s? (y/n): ", iterator->data);
         ans = getChoice();
 
+
         if (ans == 'y') {
-            printf("Got it!\n");
-        } else if (ans == 'n') {
+            printf("Got it!\n"); // Correct answer
+        } else if (ans == 'n') { // wrong asnwer
             printf("What was the country you were thinking of ? : ");
             char* new_country = (char*)malloc(MAX_LINE_SIZE * sizeof(char*));
             gets(new_country); // get new_country from the user
@@ -69,7 +76,7 @@ void play(Node* iterator, Node* original, Binary_tree countries) {
         }
         else {
             printf("Invalid input, Please try again!\n");
-            play(iterator, original, countries);
+             goto label; // try again from current node
         }
 
         // Ask to play again
@@ -77,13 +84,13 @@ void play(Node* iterator, Node* original, Binary_tree countries) {
         ans = getChoice();
 
           if (ans == 'y') {
-            play(original, original, countries);
+            play(original, original, countries); // replay the game
         }else if (ans == 'n') {
-                endGame();
+                endGame(); // end of game function
           }
           else{
               printf("Invalid input, Please try again!\n");
-              play(iterator, original, countries);
+              goto label; // try again from current node
         }
     }
 
@@ -98,7 +105,7 @@ void play(Node* iterator, Node* original, Binary_tree countries) {
             play(iterator->n, original, countries);
         } else {
             printf("Invalid input, Please try again!\n");
-            play(iterator, original, countries);
+            goto label; // try again from current node
         }
     }
 }
